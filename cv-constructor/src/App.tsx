c4d3a1d11e036a35d7869621323732139be11bb2
Anastasiya Sachko <naztya12323@gmail.com>
2022-10-07 23:38:48 +0300

    feat: add routes

--- cv-constructor/src/App.tsx
+++ cv-constructor/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
 import Global from './styles/Global';
@@ -8,7 +9,13 @@ function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <Global />
-      <div>App</div>
+      <Routes>
+        <Route path="/" element={<div>App</div>} />
+        <Route path="/constructor" element={<div>constructor</div>} />
+        <Route path="/constructor/cv" element={<div>CV</div>} />
+        <Route path="/my-cv" element={<div>my cv</div>} />
+        <Route path="*" element={<div>not found</div>} />
+      </Routes>
     </ThemeProvider>
   );
 }
