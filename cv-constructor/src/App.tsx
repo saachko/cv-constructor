0aabc2aa1e50b54795a21211cf10a55d76933227
Anastasiya Sachko <naztya12323@gmail.com>
2022-10-07 22:37:03 +0300

    feat: add and implement default theme and Global styles settings

--- cv-constructor/src/App.tsx
+++ cv-constructor/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import Global from './styles/Global';
+import defaultTheme from './styles/theme';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Global />
+      <div>App</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
